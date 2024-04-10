bceb5d218a793759d1accf94e77e7abcdbca452f
Vitor Ribeiro <Vitorribeiro20022@gmail.com>
2024-04-10 10:21:10 +0100

    up

---
+++ Aula5/Aula5/Aula5/Janela.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aula5
+{
+    /// <summary>
+    /// Lógica interna para Janela.xaml
+    /// </summary>
+    public partial class Janela : Window
+    {
+
+        
+        public Janela()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceitar_Click(object sender, RoutedEventArgs e)
+        {
+            //FiguraNova = new Figura();
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ Aula5/Aula5/Aula5/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aula5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Figura> _lista = new List<Figura>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemAdicionar_Click(object sender, RoutedEventArgs e)
+        {
+            Janela _janela = new Janela();
+
+            if(_janela.ShowDialog() == true)
+            {
+                //_lista.Add(_janela.FiguraNova);
+                //lbFiguras.Items.Add(_janela.FiguraNova.Nome);
+            }
+
+        }
+    }
+}
