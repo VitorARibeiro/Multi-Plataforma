c0cf67b198a0ca70b630e1a2091088344fc8aa7a
Vitor Ribeiro <Vitorribeiro20022@gmail.com>
2024-04-12 11:54:20 +0100

    up

---
+++ Playlist Youtube/MiniTextEditor/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MiniTextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string FilePath = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MenuOpenClick(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Title = ("Pls Pick a file"); //aparece no titulo do picker
+            ofd.Filter = "Text Files | *.txt;*.c"; //so aparece .txt
+            bool? success = ofd.ShowDialog(); // para abrir o file picker
+
+
+            if (success == true)
+            {
+                FilePath = ofd.FileName;
+                TxtBox.Text = File.ReadAllText(FilePath);
+            }
+        }
+        private void MenuSaveClick(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtBox.Text))
+            {
+                MessageBox.Show("Ficheiro Vazio", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return ;
+            }
+
+            try
+            {
+                File.WriteAllText(FilePath, TxtBox.Text);
+                MessageBox.Show("File saved successfully.");
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"An error occurred while saving the file: {ex.Message}");
+            }
+
+        }
+        private void MenuCloseClick(object sender, EventArgs e)
+        {
+            TxtBox.Text = null;
+            FilePath = "";
+
+        }
+        private void MenuExitClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Playlist Youtube/MiniTextEditor/Models/MenuBar.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Controls;
+
+namespace MiniTextEditor.Models
+{
+    /// <summary>
+    /// Interação lógica para MenuBar.xam
+    /// </summary>
+    public partial class MenuBar : UserControl
+    {
+        public event EventHandler OpenClicked;
+        public event EventHandler CloseClicked;
+        public event EventHandler SaveClicked;
+        public event EventHandler ExitClicked;
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void CloseFile(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CloseClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void SaveFile(object sender, System.Windows.RoutedEventArgs e)
+        {
+            SaveClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void Exit(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ExitClicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
